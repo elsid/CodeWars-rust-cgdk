269fb01bb81e706e01ebcc31bab94e3b89f72153
elsid <elsid.mail@gmail.com>
2017-11-17 18:29:06 +0300

    Implement https://pastebin.com/raw/Yg5PJscZ
    See https://github.com/Russian-AI-Cup-2017/cpp-cgdk/pull/3

--- src/my_strategy.rs
+++ src/my_strategy.rs
@@ -1,8 +1,17 @@
 use model::{ActionType, Game, Move, Player, World};
 use strategy::Strategy;
 
-#[derive(Default)]
-pub struct MyStrategy;
+pub struct MyStrategy {
+    iter: i32,
+}
+
+impl Default for MyStrategy {
+    fn default() -> Self {
+        MyStrategy {
+            iter: 0,
+        }
+    }
+}
 
 impl Strategy for MyStrategy {
     fn move_(&mut self, me: &Player, world: &World, game: &Game, move_: &mut Move) {
@@ -15,8 +24,32 @@ impl Strategy for MyStrategy {
 
         if world.tick_index == 1 {
             move_.action = ActionType::Move;
-            move_.x = world.width / 2.0;
-            move_.y = world.height / 2.0;
+            move_.x = 500.0;
+            move_.y = 0.0;
+            move_.max_speed = 0.01;
+            return;
         }
+
+        if me.remaining_action_cooldown_ticks > 0 || world.tick_index % 5 != 0 {
+            return;
+        }
+
+        self.iter += 1;
+
+        if self.iter <= 99 {
+            move_.action = ActionType::Assign;
+            move_.group = self.iter;
+            return;
+        }
+
+        let k = if self.iter % 2 == 0 {
+            -1.0
+        } else {
+            1.0
+        };
+
+        move_.action = ActionType::Move;
+        move_.x = k * world.width / 2.0;
+        move_.y = k * world.height / 2.0;
     }
 }
